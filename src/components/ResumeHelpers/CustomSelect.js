4befc5bf75c15d1fb03a2a276d9c69b1e61266d4
gokul0317 <gokulkrishnan.p2@gmail.com>
2021-03-24 15:20:37 +0530

    Fix: auto complete not typeable and searchable issue

--- src/components/ResumeHelpers/CustomSelect.js
+++ src/components/ResumeHelpers/CustomSelect.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Autocomplete from 'react-autocomplete';
 import skills from './skills.json'
 const CustomAutocomplete = field => {
+    const [currentSkills, setCurrentSkills] = useState(skills)
+
     return (
         <Autocomplete
             getItemValue={(item) => item.label}
-            items={skills}
+            items={currentSkills}
             renderItem={(item, isHighlighted) =>
                 <div style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
                     {item.label}
                 </div>
             }
+            onChange={value => {
+                field.input.onChange(value)
+                setCurrentSkills(() => {
+                    const filtered = skills.filter((elm) => elm.label.includes(value.target?.value))
+                    return filtered
+                })
+            }}
             value={field.input.value}
             onSelect={value => field.input.onChange(value)}
         />
